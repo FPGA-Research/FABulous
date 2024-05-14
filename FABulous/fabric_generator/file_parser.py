90405dfa83d7f9db94c7a83ec69d09646691b3bb
Jonas K <j0nask@proton.me>
2024-05-14 17:26:12 +0200

    fabric_generator:file_parser: Remove commented code and unused main
    Remove the unneeded main function and some commented code.
    Sort imports.
    
    Signed-off-by: Jonas K. <j0nask@proton.me>

--- FABulous/fabric_generator/file_parser.py
+++ FABulous/fabric_generator/file_parser.py
@@ -4,7 +4,7 @@ import re
 from loguru import logger
 from copy import deepcopy
 
-
+from typing import Literal
 from FABulous.fabric_definition.Bel import Bel
 from FABulous.fabric_definition.Port import Port
 from FABulous.fabric_definition.Wire import Wire
@@ -13,7 +13,6 @@ from FABulous.fabric_definition.SuperTile import SuperTile
 from FABulous.fabric_definition.Fabric import Fabric
 from FABulous.fabric_definition.ConfigMem import ConfigMem
 from FABulous.fabric_generator.utilities import parseList, parseMatrix
-from typing import Literal
 from FABulous.fabric_definition.define import (
     IO,
     Direction,
@@ -21,10 +20,6 @@ from FABulous.fabric_definition.define import (
     ConfigBitMode,
     MultiplexerStyle,
 )
-
-
-# from fabric import Fabric, Port, Bel, Tile, SuperTile, ConfigMem
-# from fabric import IO, Direction, Side, MultiplexerStyle, ConfigBitMode
 
 
 oppositeDic = {"NORTH": "SOUTH", "SOUTH": "NORTH", "EAST": "WEST", "WEST": "EAST"}
@@ -290,13 +285,20 @@ def parseTiles(fileName: str) -> tuple[list[Tile], list[tuple[str, str]]]:
                 ports.append(Port(Direction[temp[0]], temp[1], int(
                     temp[2]), int(temp[3]), temp[4], int(temp[5]), temp[1], IO.OUTPUT, Side[temp[0]]))
 
-                ports.append(Port(Direction[temp[0]], temp[1], int(
-                    temp[2]), int(temp[3]), temp[4], int(temp[5]), temp[4], IO.INPUT, Side[oppositeDic[temp[0]].upper()]))
-                # wireCount = (abs(int(temp[2])) +
-                #              abs(int(temp[3])))*int(temp[5])
-                # for i in range(wireCount):
-                commonWirePair.append(
-                    (f"{temp[1]}", f"{temp[4]}"))
+                ports.append(
+                    Port(
+                        Direction[temp[0]],
+                        temp[1],
+                        int(temp[2]),
+                        int(temp[3]),
+                        temp[4],
+                        int(temp[5]),
+                        temp[4],
+                        IO.INPUT,
+                        Side[oppositeDic[temp[0]].upper()],
+                    )
+                )
+                commonWirePair.append((f"{temp[1]}", f"{temp[4]}"))
 
             elif temp[0] == "JUMP":
                 ports.append(Port(Direction.JUMP, temp[1], int(
@@ -967,17 +969,3 @@ def parseConfigMem(
                 )
 
     return configMemEntry
-
-
-if __name__ == "__main__":
-    # result = parseFabricCSV('fabric.csv')
-    # result1 = parseList('RegFile_switch_matrix.list', collect="source")
-    # result = parseFileVerilog('./LUT4c_frame_config_dffesr.v')
-
-    result2 = parseFileVerilog("./test.txt")
-    # print(result[0])
-    # print(result[1])
-    # print(result[2])
-    # print(result[3])
-
-    # print(result.tileDic["W_IO"].portsInfo)
