f0f8ab453f3a16f27aa36acb15aec77be795901b
Kelvin Chung <king.chung@manchester.ac.uk>
2025-05-29 11:05:36 +0100

    add missing exception

---
+++ FABulous/custom_exception.py
@@ -0,0 +1,11 @@
+class CommandError(Exception):
+    """Exception raised for errors in the command execution."""
+    pass
+
+class EnvironmentNotSet(Exception):
+    """Exception raised when the environment is not set."""
+    pass
+
+class FileTypeError(Exception):
+    """Exception raised for unsupported file types."""
+    pass
