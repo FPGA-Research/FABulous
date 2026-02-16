85803e56888e9ba62e7defef613663c0abaa2bdc
kelvin <king.chung@manchester.ac.uk>
2026-02-16 18:44:59 +0000

    chore: remove stale patch

--- nix/overlay/nix-eda.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-
-  openroad = prev.openroad.overrideAttrs (finalAttrs: previousAttrs: {
-    patches = (previousAttrs.patches or []) ++ [../patches/openroad/fix_connect_by_abutment.patch];
-  });
-
-}
