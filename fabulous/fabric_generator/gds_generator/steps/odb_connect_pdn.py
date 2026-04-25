d438ff10ad3ba8a6e4156378f4ff39556e16fbde
Marc-André Tétrault <marc-andre.tetrault@usherbrooke.ca>
2026-04-25 11:39:06 +0200

    fixed multilayer pdn changed hardcoded power nets for VDD_NETS/GND_NETS Automatic metal layer selection --> multilayer pdn Initial support for multiple power domains (no test case yet)

--- fabulous/fabric_generator/gds_generator/steps/odb_connect_pdn.py
+++ fabulous/fabric_generator/gds_generator/steps/odb_connect_pdn.py
@@ -1,10 +1,14 @@
 """FABulous GDS Generator - ODB Power Connection Step."""
 
 from importlib import resources
+from typing import Tuple
 
+from librelane.config.flow import option_variables
 from librelane.steps.common_variables import pdn_variables
 from librelane.steps.odb import OdbpyStep
-from librelane.steps.step import Step
+from librelane.steps.step import Step, ViewsUpdate, MetricsUpdate
+from librelane.state.state import DesignFormat, State
+
 
 
 @Step.factory.register()
@@ -14,7 +18,7 @@ class FABulousPDN(OdbpyStep):
     id = "Odb.FABulousPDN"
     name = "FABulous PDN connections for the tiles"
 
-    config_vars = pdn_variables
+    config_vars = pdn_variables + option_variables
 
     def get_script_path(self) -> str:
         """Get the path to the power connection script."""
@@ -23,9 +27,18 @@ class FABulousPDN(OdbpyStep):
             / "odb_power.py"
         )
 
-    def get_command(self) -> list[str]:
-        """Get the command to run the power connection script."""
-        return super().get_command() + [
-            "--metal-layer-name",
-            self.config["PDN_VERTICAL_LAYER"],
-        ]
+    def run(self, state_in: State, **kwargs) -> Tuple[ViewsUpdate, MetricsUpdate]:
+        kwargs, env = self.extract_env(kwargs)
+
+        # default values for FABulousPDN
+        if self.config["VDD_NETS"] == None:
+            env["VDD_NETS"] = "VPWR"
+        else:
+            env["VDD_NETS"] = ''.join(self.config["VDD_NETS"])
+
+        if self.config["GND_NETS"] == None:
+            env["GND_NETS"] = "VGND"
+        else:
+            env["GND_NETS"] = ''.join(self.config["GND_NETS"])
+
+        return super().run(state_in, env=env, **kwargs)
