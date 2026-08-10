98158cd7aa0a67da55d585a7886b7b0034de60c6
MrAbhi19 <214151262+MrAbhi19@users.noreply.github.com>
2026-08-11 01:00:24 +0530

    Fixed Pre-commit errors

--- fabulous/fabulous_settings.py
+++ fabulous/fabulous_settings.py
@@ -36,8 +36,8 @@ from fabulous.fabric_definition.define import HDLType
 FAB_USER_CONFIG_DIR = Path(typer.get_app_dir("FABulous", force_posix=True))
 MODELS_PACK_REQUIRED_MODULES: list[str] = [
     "config_latch",
-    "config_dff"
     "my_buf",
+    "config_dff",
     "clk_buf",
     "cus_mux41",
     "cus_mux21",
