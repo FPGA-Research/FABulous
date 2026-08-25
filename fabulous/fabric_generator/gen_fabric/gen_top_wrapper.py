d13a598fee7fd9f3e3a0d305678732542578c58a
MrAbhi19 <214151262+MrAbhi19@users.noreply.github.com>
2026-08-25 21:52:25 +0530

    added logging for skipping top wrapper generation in FlipFlopChain mode

--- fabulous/fabric_generator/gen_fabric/gen_top_wrapper.py
+++ fabulous/fabric_generator/gen_fabric/gen_top_wrapper.py
@@ -9,6 +9,8 @@ interfaces.
 import re
 from pathlib import Path
 
+from loguru import logger
+
 from fabulous.fabric_definition.define import IO, ConfigBitMode
 from fabulous.fabric_definition.fabric import Fabric
 from fabulous.fabric_generator.code_generator.code_generator import CodeGenerator
@@ -27,6 +29,7 @@ def generateTopWrapper(writer: CodeGenerator, fabric: Fabric) -> None:
     This includes features that are not located inside the fabric such as BRAM.
     """
     if fabric.configBitMode == ConfigBitMode.FLIPFLOP_CHAIN:
+        logger.info("Notice: Top wrapper generation is skipped for FlipFlopChain mode.")
         return
 
     def split_port(p: str) -> tuple[tuple[int, int], tuple[int, ...], str]:
